2534f686c6fb583d04e5046892d6b4050e37a784
Xavier Trouche <xavier.trouche@ecirtp.fr>
2024-07-28 17:02:24 +0200

    👌 IMPROVE: test vue counter

--- assets/vue/controllers/Hello.vue
+++ assets/vue/controllers/Hello.vue
@@ -1,11 +1,30 @@
-<template>
-    <div>
-        <h1 class="text-red-600">Hello {{ name }}!</h1>
-    </div>
-</template>
+<script>
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {
+      count: 0
+    }
+  },
 
-<script setup>
-    defineProps({
-        name: String
-    });
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
 </script>
+
+<template>
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" @click="increment">Count is: {{ count }}</button>
+</template>
